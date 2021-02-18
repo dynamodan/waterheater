1801c6600fb00b3ead82332774108b7aa8f2e213
Dynamo Dan <info@dynamdoan.com>
2021-02-18 13:44:29 -0500

    Implemented HD44780 display, 30mV thermocouple op-amp amplifier with 100x gain and corresponding arithmetic

--- WaterHeater.ino
+++ WaterHeater.ino
@@ -6,17 +6,19 @@
  
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
+ * LCD Hitachi 44780 compatible attached to digital pins 8,9,5,4,3,2
+ * Various sensors attached to analog pins
  
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Nov 2013
+ 
+ created February 18, 2021
  by Dan Hartman
- 
+  
  */
 
 #include <SPI.h>
 #include <Ethernet.h>
+#include <LiquidCrystal.h>
+
 
 // thermistor and voltage divider characteristics:
 #define THERMISTORNOMINAL 10000
@@ -27,30 +29,145 @@
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,1,98);
+IPAddress ip(192,168,1,96);
 
 // these are for the analogReads:
 unsigned int fireTemp = 0;
 int waterTemp1 = 0;
 int waterTemp2 = 0;
+unsigned long lastMillis = 0;
+int fireStarting = 0;
+
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use 
 // (port 80 is default for HTTP):
 EthernetServer server(8000);
 
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 9, en = 8, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
 void setup()
 {
-  analogReference(EXTERNAL); // use a forward-biased silicon diode for 0.6-0.7v
-  pinMode(5, INPUT_PULLUP); // weak pullup (this is checked for LOW later for debugging)
+  // set up the LCD's number of columns and rows:
+  lcd.begin(20, 4);
+  // Print a message to the LCD.
+  lcd.print("Two-Tank Wood Burner");
+  lcd.setCursor(0, 1);
+  lcd.print("T1:       T2:       ");
+  lcd.setCursor(0, 2);
+  lcd.print("Fire:         Dft:  ");
 
+  analogReference(DEFAULT); // use 5v reference
+
+  // set pin 6 as PWM out, and set to 50% output:
+  // (this will drive the charge pump for the
+  // instrumentation amp's negative supply)
+  pinMode(6, OUTPUT);
+  analogWrite(6, 128);
+
+  // set pin 7 as digital output, for opening/closing the draft:
+  pinMode(7, OUTPUT);
+
+  
+  
+  // setup timer interrupt for handling events, writing LCD, etc:
+  OCR0A = 0xAF;
+  TIMSK0 |= _BV(OCIE0A);
+  lastMillis = millis();
   
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
   server.begin();
+ 
+}
 
+// one-second loop here:
+SIGNAL(TIMER0_COMPA_vect) {
+  unsigned long currentMillis = millis();
+  // once every second:
+  if(currentMillis - lastMillis > 1000) {
+    // print the T1 and T2 temp:
+    // reading water temps:
+    waterTemp1 = analogRead(1);
+    waterTemp2 = analogRead(2);
+
+    // see if the user called for draft at starting or stoking:
+    if(analogRead(5) < 500) {
+      fireStarting = 900;
+    }
+    if(fireStarting > 0) { fireStarting--; }
+    
+    // take an average to determine what we should do about draft:
+    // float waterTempF = ohmsToF((waterTemp1 + waterTemp2) / 2);
+    float waterTempF = ohmsToF(waterTemp2); // or not...
+    
+    lcd.setCursor(4, 1);
+    if(waterTemp1 > 1000) {
+      lcd.print("+++  ");
+    } else if(waterTemp1 < 10) {
+      lcd.print("---  ");
+    } else {
+      lcd.print(ohmsToF(waterTemp1));
+      lcd.print("F");
+    }
+    lcd.setCursor(14, 1);
+    if(waterTemp2 > 1000) {
+      lcd.print("+++  ");
+    } else if(waterTemp2 < 10) {
+      lcd.print("---  ");
+    } else {
+      lcd.print(ohmsToF(waterTemp2));
+      lcd.print("F");
+    }
+    
+    // print the Fire temp:
+    fireTemp = analogRead(0);
+    if(fireTemp > 0) {
+      float fireTempF = (fireTemp / .54) + 70;
+      lcd.setCursor(5, 2);
+      lcd.print(fireTempF);
+      lcd.print("F ");
+    } else {
+      lcd.setCursor(5, 2);
+      lcd.print("---    ");
+    }
+
+    // do draft control logic:
+    // is there a fire?
+    lcd.setCursor(18, 2);
+    if(fireTemp > 50 && waterTempF < 140) { // fire is going, and we want hotter
+      // open the draft:
+      digitalWrite(7, HIGH);
+      lcd.print("F+");
+    } else if(waterTempF > 160) { // water is too hot, we want to slow any fires
+      digitalWrite(7, LOW);
+      lcd.print("F-");
+    } else if(fireStarting > 0) { // we are starting or stoking fire and want draft open
+      digitalWrite(7, HIGH);
+      lcd.print("FS");
+    } else {
+      digitalWrite(7, LOW); // fire is out, we want to close draft to hold heat
+      lcd.print("--");
+    }
+    
+    
+    
+    // print the number of seconds since reset:
+    lcd.setCursor(0, 3);
+    lcd.print("millis: ");
+    lcd.print(millis());
+    lcd.print("  ");
+    lastMillis = currentMillis;
+  }
   
-  
+  // when millis() overflows:
+  else if(currentMillis < lastMillis) {
+    lastMillis = currentMillis;
+  }
 }
 
 void debugSerial() {
@@ -89,9 +206,9 @@ void debugSerialLoop() {
 void loop()
 {
   
-  if(digitalRead(5) == LOW) {
-    debugSerial(); // this never returns, you have to unground pin 5, and reset. 
-  }
+  // if(digitalRead(5) == LOW) {
+  //  debugSerial(); // this never returns, you have to unground pin 5, and reset. 
+  //}
   
   
   // listen for incoming clients
@@ -246,4 +363,3 @@ void stylePrint(EthernetClient &client, unsigned int fireTemp) {
   
   client.println(".main{height:100%;width:100%;} .big{font-size:40px;} input{width:100px;height:75px;}</style>");
 }
-
